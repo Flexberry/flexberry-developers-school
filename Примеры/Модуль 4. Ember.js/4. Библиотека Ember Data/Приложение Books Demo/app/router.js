63585749622df13e5d1251d7088384faf53f154e
Evgeniy Khaberev <ehaberev@yandex.ru>
2021-02-23 16:02:01 +0500

    Add missing books demo examples

---
+++ Примеры/Модуль 4. Ember.js/4. Библиотека Ember Data/Приложение Books Demo/app/router.js
@@ -0,0 +1,28 @@
+import EmberRouter from '@ember/routing/router';
+import config from './config/environment';
+
+const Router = EmberRouter.extend({
+  location: config.locationType,
+  rootURL: config.rootURL
+});
+
+Router.map(function() {
+  this.route('author', { path: '/authors'}, function() {
+    this.route('detail', { path: '/:id'}, function() {
+      this.route('new-book');
+    });
+    this.route('create');
+    this.route('edit', { path: '/:id/edit'});
+  });
+  this.route('book', function() {
+    this.route('detail', { path: '/:id'}, function() {
+      this.route('new-review');
+    });
+    this.route('create');
+    this.route('edit', { path: '/:id/edit'});
+  });
+  this.route('error', { path: '/:error'});
+  this.route('404', { path: '*path'});
+});
+
+export default Router;
