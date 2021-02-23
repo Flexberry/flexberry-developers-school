31ff5820ba19d529bfbd74d90b3aa2edd95b7969
Evgeniy Khaberev <ehaberev@yandex.ru>
2021-02-23 15:49:53 +0500

    Add validations and internationalization for books demo

---
+++ Примеры/Модуль 4. Ember.js/6. Валидация и интернационализация/Приложение Books Demo/app/adapters/application.js
@@ -0,0 +1,34 @@
+import DS from 'ember-data';
+import { computed } from '@ember/object';
+import { inject as service } from '@ember/service';
+import ENV from 'books-demo/config/environment';
+
+export default DS.JSONAPIAdapter.extend({
+  session: service(),
+  host: ENV.backendURL,
+
+  headers: computed(function() {
+    let resultHeaders = {
+      'Content-Type': 'application/json'
+    };
+
+    if (this.get('session.isAuthenticated')) {
+      resultHeaders['Authorization'] = `Bearer ${this.session.data.authenticated.token}`;
+    }
+
+    return resultHeaders;
+  }).volatile(),
+
+  buildURL(modelName, id, snapshot, requestType, query) {
+    let url = this._super(...arguments);
+    if (modelName === 'author' && requestType === 'findRecord' && id) {
+      url += '?_embed=books';
+    }
+
+    if (modelName === 'book' && requestType === 'findRecord' && id) {
+      url += '?_embed=reviews';
+    }
+
+    return url;
+  }
+});
