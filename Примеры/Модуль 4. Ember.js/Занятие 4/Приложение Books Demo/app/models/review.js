18577322bef913d987dae8d1496ed6f6f12f6721
Evgeniy Khaberev <ehaberev@yandex.ru>
2021-02-23 15:55:29 +0500

    Rename folders for Ember examples

--- Примеры/Модуль 4. Ember.js/Занятие 4/Приложение Books Demo/app/models/review.js
+++
@@ -1,9 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-  user: DS.attr('string'),
-  body: DS.attr('string'),
-  createdAt: DS.attr('date-string'),
-
-  book: DS.belongsTo('book')
-});
