f841aaff6aa3a942ead22a361fcb8bf605771267
Eugene Kolegov <ekolegov@outlook.com>
2021-03-27 12:13:52 +0500

    Use components and computed properties instead of observers

--- Примеры/Модуль 8/Приложение/ember-app/app/models/i-i-s-shop-order-item.js
+++ Примеры/Модуль 8/Приложение/ember-app/app/models/i-i-s-shop-order-item.js
@@ -3,9 +3,7 @@ import EmberFlexberryDataModel from 'ember-flexberry-data/models/model';
 import OfflineModelMixin from 'ember-flexberry-data/mixins/offline-model';
 import OrderStatusEnum from '../enums/i-i-s-shop-order-status';
 import { validator } from 'ember-cp-validations';
-import { observer, computed } from '@ember/object';
-import { once } from '@ember/runloop';
-import { on } from '@ember/object/evented';
+import { computed } from '@ember/object';
 
 import {
   defineProjections,
@@ -20,48 +18,23 @@ const Validations = buildValidations(ValidationRules, {
 let Model = EmberFlexberryDataModel.extend(OfflineModelMixin, OrderItemMixin, Validations, {
   taxes: 10,
 
-  /*
-   * Цена с налогом
-   */
-  _priceWTaxesChanged: on('init', observer('product', function() {
-    if (!this.get('order.isBlocked')) {
-      once(this, '_priceWTaxesCompute');
-    }
-  })),
-  _priceWTaxesCompute: function() {
-    let product = this.get('product');
-
-    let result = 0; // При добавлении строки, когда товара еще нет
-    if (product) {
-      let price = Number(product.get('price'));
-      let taxes = this.get('taxes') / 100 + 1;
-      result = Number((price * taxes).toFixed(2)); // округление до 2 знаков
+  actualPriceWTaxes: computed('product.price', 'taxes', function() {
+    const price = Number(this.get('product.price') || 0);
+    if (Number.isNaN(price)) {
+      throw new Error(`Invalid price for product: '${this.get('product')}'.`);
     }
 
-    if (!this.get('isDeleted')) {
-      this.set('priceWTaxes', result);
-    }
-  },
+    const coefficient = this.get('taxes') / 100 + 1;
 
-  /*
-   * Сумма по позиции
-   */
-  _totalSumChanged: on('init', observer('priceWTaxes', 'amount', function() {
-    once(this, '_totalSumCompute');
-  })),
-  _totalSumCompute: function() {
-    let priceWTaxes = Number(this.get('priceWTaxes'));
-    let amount = Number(this.get('amount'));
+    return (price * coefficient).toFixed(2);
+  }),
 
-    let result = 0;
-    if (!Number.isNaN(priceWTaxes) && !Number.isNaN(amount)) {
-      result = Number((priceWTaxes * amount).toFixed(2));
-    }
+  actualTotalSum: computed('actualPriceWTaxes', 'amount', function() {
+    const price = Number(this.get('actualPriceWTaxes') || 0);
+    const amount = Number(this.get('amount') || 0);
 
-    if (!this.get('isDeleted')) {
-      this.set('totalSum', result);
-    }
-  },
+    return (price * amount).toFixed(2);
+  }),
 });
 
 defineProjections(Model);
